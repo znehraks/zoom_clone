f7bab03fdb8449aecb95c48809a9aa60123b1c6a
znehraks <znehraks@gmail.com>
2021-10-21 22:19:32 +0900

    setup & join_room

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import http from "http";
+import express from "express";
+import SocketIO from "socket.io";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`listening on http://localhost:3000`);
+
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
+httpServer.listen(3000, handleListen);
